af743bcdaa7233e64dce5a11690a4f1cdfe77470
Rafael Diniz <amauri.rafael33@gmail.com>
2026-06-16 23:03:30 -0300

    add new decode time metric

--- tests/test_codec_fidelity.py
+++ tests/test_codec_fidelity.py
@@ -68,12 +68,11 @@ def test_codec_fidelity(args):
     print("PSNR: 0.0 to inf, with infinite being a perfect score.\nSSIM: 0.0 to 1.0, with 1.0 being a perfect score", end="\n\n")
     print("Quality Reference:\nPSNR >= 40 dB, SSIM >= 0.95: Excelent Fidelity\nPSNR >= 30 dB, SSIM >= 0.90: Good Fidelity\n")
 
-    print(f"{'Video Source':<20} | {'PSNR score (dB)':<20} | {'SSIM score':<20} | {'Size Original (MB)':<20} | {'Size Z-Tensor Encoded (MB)':<20} | {'Encode Time (s)':<20} | {'Decode Time (s)':<20}")
+    print(f"{'Video Source':<20} | {'PSNR score (dB)':<20} | {'SSIM score':<20} | {'Size Raw Video (MB)':<20} | {'Size Z-Tensor Encoded (MB)':<30} | {'Encode Time (s)':<20} | {'Decode Time (s)':<20}")
     print("-" * 170)
 
 
     for video_path in videos:
-
         avg_psnr, avg_ssim, size_orig_mb, size_ztensor_mb, encode_time, decode_time = run_fidelity_check(video_path, 
                                                                                args.device, 
                                                                                args.mem, 
@@ -86,6 +85,6 @@ def test_codec_fidelity(args):
                                                                                )
 
         if avg_psnr == "Lossless" and avg_ssim == "Lossless":
-            print(f"{os.path.basename(video_path):<30} | {'Lossless':<20} | {'Lossless':<20}  | {size_orig_mb:<20.1f} | {size_ztensor_mb:<20.1f} | {encode_time:20.1f} | {decode_time:20.1f}")
+            print(f"{os.path.basename(video_path):<20} | {'Lossless':<20} | {'Lossless':<20}  | {size_orig_mb:<20.1f} | {size_ztensor_mb:<30.1f} | {encode_time:20.1f} | {decode_time:20.1f}")
         else:
-            print(f"{os.path.basename(video_path):<30} | {avg_psnr:<20.2f} | {avg_ssim:<20.2f} | {size_orig_mb:<20.1f} | {size_ztensor_mb:<20.1f} | {encode_time:20.1f} | {decode_time:20.1f}")
+            print(f"{os.path.basename(video_path):<20} | {avg_psnr:<20.2f} | {avg_ssim:<20.2f} | {size_orig_mb:<20.1f} | {size_ztensor_mb:<30.1f} | {encode_time:20.1f} | {decode_time:20.1f}")
